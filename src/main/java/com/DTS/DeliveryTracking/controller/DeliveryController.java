d7cb5e5a0b78ae539f82e97f6fafd19f1f500fc5
Ayoub <118635029+EAP44@users.noreply.github.com>
2025-04-08 11:33:54 +0100

    Update DeliveryController.java

--- src/main/java/com/DTS/DeliveryTracking/controller/DeliveryController.java
+++ src/main/java/com/DTS/DeliveryTracking/controller/DeliveryController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/maps")
+@RequestMapping("/api")
 public class DeliveryController {
 
     private final GoogleMapsService googleMapsService;
